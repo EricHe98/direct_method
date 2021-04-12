ad9caa437eba67255367672fb03f1d30c2c9c5dc
Eric He <eric@rocketmiles.com>
2021-04-12 14:08:58 -0400

    initialize

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Evaluate the efficacy of the "direct method" of using a model to estimate missing values, counterfactuals, bandit losses and other situations of conditionally missing data',
+    author='Youngjin Tejomay Soojin Eric',
+    license='MIT',
+)
